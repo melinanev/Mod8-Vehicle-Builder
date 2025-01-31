a8a68115e54aed62b951805d88eeb8603c41dacc
melinanev <melina.l.nevarez@gmail.com>
2025-01-30 19:02:10 -0700

    Update Motorbike file
    Realized that these need to be in order and the color section was not in the order with which it would be pulled.  changed that.

--- Develop_8/src/classes/Motorbike.ts
+++ Develop_8/src/classes/Motorbike.ts
@@ -60,12 +60,12 @@ class Motorbike extends Vehicle {
   override printDetails(): void {
     super.printDetails();
     console.log(`VIN: ${this.vin}`);
+    console.log(`Color: ${this.color}`);
     console.log(`Make: ${this.make}`);
     console.log(`Model: ${this.model}`);
     console.log(`Year: ${this.year}`);
     console.log(`Weight: ${this.weight} lbs`);
     console.log(`Top Speed: ${this.topSpeed} mph`);
-    console.log(`Color: ${this.color}`);
     console.log(`Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`);
     console.log(`Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`);
   }
