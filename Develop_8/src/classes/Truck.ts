60701d55895450ccdc4f99f64709e021aa8c16e0
melinanev <melina.l.nevarez@gmail.com>
2025-03-22 16:21:17 -0600

    rewrite truck file
    also had to rewrite truck file once errors from other files found and corrected

--- Develop_8/src/classes/Truck.ts
+++ Develop_8/src/classes/Truck.ts
@@ -1,15 +1,4 @@
-// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
-import Vehicle from './Vehicle.js';
-import Motorbike from './Motorbike.js';
-import Car from './Car.js';
-import Wheel from './Wheel.js';
-import AbleToTow from '../interfaces/AbleToTow.js';
-
-// TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck extends Vehicle implements AbleToTow {
-  // TODO: Declare properties of the Truck class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+class Vehicle {
   vin: string;
   color: string;
   make: string;
@@ -18,7 +7,94 @@ class Truck extends Vehicle implements AbleToTow {
   weight: number;
   topSpeed: number;
   wheels: Wheel[];
-  towingCapacity: number;
+  started: boolean;
+  currentSpeed: number;
+
+  constructor(
+    vin: string, 
+    color: string, 
+    make: string, 
+    model: string, 
+    year: number, 
+    weight: number, 
+    topSpeed: number, 
+    wheels: Wheel[]
+  ) {
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+    this.started = false;
+    this.currentSpeed = 0;
+  }
+
+  printDetails(): void {
+    console.log(`Vehicle Details: ${this.make} ${this.model}`);
+  }
+
+  start(): void {
+    this.started = true;
+    console.log(`Vehicle ${this.make} ${this.model} has started.`);
+  }
+
+  accelerate(speed: number): void {
+    if (this.started) {
+      this.currentSpeed += speed;
+      console.log(`Vehicle ${this.make} ${this.model} accelerated to ${this.currentSpeed} mph`);
+    } else {
+      console.log('Start the vehicle first');
+    }
+  }
+
+  decelerate(speed: number): void {
+    if (this.started) {
+      this.currentSpeed -= speed;
+      console.log(`Vehicle ${this.make} ${this.model} decelerated to ${this.currentSpeed} mph`);
+    } else {
+      console.log('Start the vehicle first');
+    }
+  }
+
+  stop(): void {
+    this.currentSpeed = 0;
+    this.started = false;
+    console.log(`Vehicle ${this.make} ${this.model} has stopped.`);
+  }
+
+  turn(direction: string): void {
+    if (this.started) {
+      console.log(`Vehicle ${this.make} ${this.model} turned ${direction}.`);
+    } else {
+      console.log('Start the vehicle first');
+    }
+  }
+}
+
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+
+
+// TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
+class Truck extends Vehicle implements AbleToTow {
+  // TODO: Declare properties of the Truck class
+  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+  override vin!: string;
+  override color!: string;
+  override make!: string;
+  override model!: string;
+  override year!: number;
+  override weight!: number;
+  override topSpeed!: number;
+  override wheels!: Wheel[];
+  towingCapacity!: number;
 
   // TODO: Create a constructor that accepts the properties of the Truck class
     // TODO: The constructor should call the constructor of the parent class, Vehicle
@@ -37,15 +113,7 @@ class Truck extends Vehicle implements AbleToTow {
 
 
   { 
-    super ();
-    this.vin = vin;
-    this.color = color;
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.weight = weight;
-    this.topSpeed = topSpeed;
-    this.wheels = wheels;
+    super(vin, color, make, model, year, weight, topSpeed, wheels);
     this.towingCapacity = towingCapacity;
 
     if (wheels.length !== 4) {
@@ -55,18 +123,63 @@ class Truck extends Vehicle implements AbleToTow {
     }
   }
 
+  // Override the start method from the Vehicle class
+  override start(): void {
+    console.log(`Truck ${this.make} ${this.model} has started.`);
+    super.start();
+  }
+
+  // Override the accelerate method from the Vehicle class
+  override accelerate(speed: number): void {
+    if (this.started) {
+      this.currentSpeed += speed;
+      console.log(`Truck ${this.make} ${this.model} accelerated to ${this.currentSpeed} mph`);
+    } else {
+      console.log('Start the truck first');
+    }
+  }
+
+  // Override the decelerate method from the Vehicle class
+  override decelerate(speed: number): void {
+    if (this.started) {
+      this.currentSpeed -= speed;
+      console.log(`Truck ${this.make} ${this.model} decelerated to ${this.currentSpeed} mph`);
+    } else {
+      console.log('Start the truck first');
+    }
+  }
+
+  // Override the stop method from the Vehicle class
+  override stop(): void {
+    this.currentSpeed = 0;
+    this.started = false;
+    console.log(`Truck ${this.make} ${this.model} has stopped.`);
+  }
+
+  // Override the turn method from the Vehicle class
+  override turn(direction: string): void {
+    if (this.started) {
+      console.log(`Truck ${this.make} ${this.model} turned ${direction}.`);
+    } else {
+      console.log('Start the truck first');
+    }
+  }
+
+  // Implement the reverse method
+  reverse(): void {
+    if (this.started) {
+      console.log(`Truck ${this.make} ${this.model} is reversing.`);
+    } else {
+      console.log('Start the truck first');
+    }
+  }
+
   // TODO: Implement the tow method from the AbleToTow interface
-  tow(vehicle: Truck | Motorbike | Car): void {
-    // TODO: Get the make an model of the vehicle if it exists
-    // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    // TODO: If it is, log that the vehicle is being towed
-    // TODO: If it is not, log that the vehicle is too heavy to be towed
-    if (vehicle instanceof Truck || vehicle instanceof Car || vehicle instanceof Motorbike) {
-      if (vehicle.weight <= this.towingCapacity) {
-        console.log(`${vehicle.make} ${vehicle.model} is being towed.`);
-      } else {
-        console.log(`${vehicle.make} ${vehicle.model} is too heavy to be towed.`);
-      }
+  tow(vehicle: Vehicle): void {
+    if (vehicle.weight <= this.towingCapacity) {
+      console.log(`${this.make} ${this.model} is towing ${vehicle.make} ${vehicle.model}.`);
+    } else {
+      console.log(`${vehicle.make} ${vehicle.model} is too heavy to be towed.`);
     }
   }
 
